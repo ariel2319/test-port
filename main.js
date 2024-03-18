2eda852d6e93c37b12350cc403a0f14aa783d86f
ariel2319 <maldonadoaa.91@gmail.com>
2024-03-18 00:54:55 -0300

    navbar animation

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const hiddenElements = document.querySelectorAll(".hidden")
 //! utilizamos el ALL para que seleccione todos los elementos con esa clase y no solo el primero
+const links = document.querySelectorAll(".link");
+
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       /* console.log(entry.target) */
       entry.target.classList.add("show");
+
+      links.forEach((link) => {
+        /* console.log(link); */
+        const href = link.getAttribute("href").split('#')[1];
+        const id = entry.target.id;
+
+        if (href === id) {
+          link.classList.add("link-active")
+        } else {
+          link.classList.remove("link-active")
+        }
+      })
     } else {
       entry.target.classList.remove("show");
     }
